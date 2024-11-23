bf07e826b1caa1cbb3aa960642e65b1f5cb1a2a7
Prasoon <prasoonmishra9936@gmail.com>
2024-11-23 13:16:09 +0530

    5th commit

--- frontend/src/History/History.js
+++ frontend/src/History/History.js
@@ -14,20 +14,22 @@ function History() {
                 const {_id, title, amount, type} = item
                 return (
                     <div key={_id} className="history-item">
-                        <p style={{
-                            color: type === 'expense' ? 'red' : 'var(--color-green)'
-                        }}>
-                            {title}
-                        </p>
+    <p style={{
+        color: type === 'expense' ? 'red' : 'var(--color-green)'
+    }}>
+        {title}
+    </p>
 
-                        <p style={{
-                            color: type === 'expense' ? 'red' : 'var(--color-green)'
-                        }}>
-                            {
-                                type === 'expense' ? `-${amount <= 0 ? 0 : amount}` : `+${amount <= 0 ? 0: amount}`
-                            }
-                        </p>
-                    </div>
+    <p style={{
+        color: type === 'expense' ? 'red' : 'var(--color-green)'
+    }}>
+        {
+            type === 'expense' 
+            ? `-₹${amount <= 0 ? 0 : amount}` 
+            : `+₹${amount <= 0 ? 0: amount}`
+        }
+    </p>
+</div>
                 )
             })}
         </HistoryStyled>
